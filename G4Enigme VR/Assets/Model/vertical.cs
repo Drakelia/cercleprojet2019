7e87cdb89d3fef541beaf0900b04c2a2579cca93
Colin <colin.cinq@gmail.com>
2019-11-07 05:52:26 +0100

    mini balance + script + objet et mini asset pack

---
+++ G4Enigme VR/Assets/Model/vertical.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vertical : MonoBehaviour
+{
+    float timeLeft, hDep;
+    GameObject door;
+
+    private void Start()
+    {
+        timeLeft = 3;
+        door = GameObject.Find("moveBlock");
+        hDep = door.transform.position.y;
+    }
+    void Update()
+    {
+        float angle = transform.eulerAngles.z;
+        print(angle);
+        if(angle>359.5 || angle < 0.5) {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0) { 
+                door.transform.Translate(0, Time.deltaTime, 0);
+            }
+        }else {
+            timeLeft = 3;
+            if(door.transform.position.y > hDep) {
+                door.transform.Translate(0, -Time.deltaTime, 0);
+            }
+           
+        }
+    }
+
+}
