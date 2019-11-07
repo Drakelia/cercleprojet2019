8073b398556abcb7ffa559ba954bd8a1c75fbfcc
Lisa <lisa.seigle-morier@lmdsio.fr>
2019-11-07 18:20:52 +0100

    Timer

---
+++ G4Enigme VR/Assets/Script/timerScriptt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime;
+
+public class timerScriptt : MonoBehaviour
+{
+
+    private int timer = 60 * 15;
+    private double TimerAffiche;
+    int timerSeconde;
+    string TimerAfficheString;
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("Time") == 9999 || PlayerPrefs.GetInt("Time") <= 0) {
+            print("ok");
+        } else {
+            timer = PlayerPrefs.GetInt("Time");
+        } 
+
+        StartCoroutine(toto());
+    }
+
+    IEnumerator toto()
+    {
+        yield return new WaitForSeconds(1);
+
+        SetTimer();
+    }
+    
+    private void SetTimer()
+    {
+        timer--;
+        PlayerPrefs.SetInt("Time", 900);
+        TimerAffiche = (int) timer / 60; // nombre virgule chelou
+        timerSeconde = timer % 60;
+        print(TimerAffiche);
+
+
+        //Debug.Log(timer);
+        gameObject.GetComponent<Text>().text = TimerAffiche.ToString() + ":" + timerSeconde.ToString() ;
+
+        if (timer == 0) return;
+
+        StartCoroutine(toto());
+           
+    }
+
+}
