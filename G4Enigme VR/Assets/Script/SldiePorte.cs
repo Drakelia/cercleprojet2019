003247638ffc376d4f25e8a7642c174d26147ce9
SEB69004 <sebastienaoura@gmail.com>
2019-11-07 10:40:48 +0100

    Map final

--- G4Enigme VR/Assets/Script/SldiePorte.cs
+++ G4Enigme VR/Assets/Script/SldiePorte.cs
@@ -11,20 +11,28 @@ public class SldiePorte : MonoBehaviour
     bool Pot2;
     bool Pot3;
     bool Pot4;
-    bool PorteOuverte;
+    bool PorteOuverte = false;
+    public float speed;
+
 
 
     int fps = 0;
-    
+
     void Start()
     {
+  
         _animator = GetComponent<Animator>();
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+ 
+
         float posXPos1 = GameObject.Find("Pot1").transform.position.x;
         float posYPos1 = GameObject.Find("Pot1").transform.position.z;
         float posXPos2 = GameObject.Find("Pot2").transform.position.x;
@@ -34,20 +42,21 @@ public class SldiePorte : MonoBehaviour
         float posXPos4 = GameObject.Find("Pot4").transform.position.x;
         float posYPos4 = GameObject.Find("Pot4").transform.position.z;
 
-        if ( posXPos1 > 295 && posXPos1 < 299 && posYPos1 > 45  && posYPos1 < 47)
+        if (posXPos1 > 295 && posXPos1 < 299 && posYPos1 > 45 && posYPos1 < 47)
         {
             Pot1 = true;
             print("pot1 ok");
-        } 
+        }
 
 
         if (posXPos2 > 295 && posXPos2 < 300 && posYPos2 > 53 && posYPos2 < 56)
         {
             Pot2 = true;
             print("pot2 ok");
-        } else
+        }
+        
 
-        if (posXPos3 < 310 && posXPos3 > 306 && posYPos3 > 53 && posYPos3 < 56 )
+        if (posXPos3 < 310 && posXPos3 > 306 && posYPos3 > 53 && posYPos3 < 56)
         {
             Pot3 = true;
             print("pot3 ok");
@@ -61,21 +70,23 @@ public class SldiePorte : MonoBehaviour
         }
 
 
-
-
-        if (Pot1 && Pot2 && Pot3 && Pot4 && fps < 250) { 
-             fps = fps + 1;
-            _animator.gameObject.GetComponent<Animator>().enabled = true;
-            _animator.SetBool("portes", true);
-            print(fps);
-            
-         } else {
-
-            fps = 0;
-            _animator.gameObject.GetComponent<Animator>().enabled = false;
-            _animator.SetBool("portes", false);
-
+        if (Pot1 && Pot2 && Pot3 && Pot4 /*&& fps <= 330*/ )
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
         }
-
+            /*           fps = fps + 1;
+                       _animator.gameObject.GetComponent<Animator>().enabled = true;
+                       _animator.SetBool("portes", true);
+                       print(fps);
+                   }
+                    else if (Pot1 && Pot2 && Pot3 && Pot4 && fps == 331 )
+                     {
+                         fps = fps + 1;
+                         _animator.gameObject.GetComponent<Animator>().enabled = false;
+                         _animator.SetBool("portes", false);
+                         print(fps);
+                     }
+            */
+        }
     }
-}
+
