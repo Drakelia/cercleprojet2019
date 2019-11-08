68f16d3e8fa6fc3c26c503720af1dbf98f93c23a
SEB69004 <sebastienaoura@gmail.com>
2019-11-08 09:57:02 +0100

    fix

---
+++ G4Enigme VR/Assets/Scripts/Lancement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lancement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+           PlayerPrefs.SetInt("Time",900);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ G4Enigme VR/Assets/Scripts/timerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime;
+
+public class timerScriptt : MonoBehaviour
+{
+
+    private int timer;
+    private double TimerAffiche;
+    int timerSeconde;
+    private void Start()
+    {
+        print("je passe dedant");
+        timer = PlayerPrefs.GetInt("Time");
+        StartCoroutine(toto());
+    }
+
+    IEnumerator toto()
+    {
+        yield return new WaitForSeconds(1);
+
+        SetTimer();
+    }
+    
+    private void SetTimer()
+    {
+        print("Je passe");
+        timer--;
+        PlayerPrefs.SetInt("Time", timer);
+        TimerAffiche = (int) timer / 60; // nombre virgule chelou
+        timerSeconde = timer % 60;
+        print(TimerAffiche);
+
+
+        //Debug.Log(timer);
+        gameObject.GetComponent<Text>().text = TimerAffiche.ToString() + ":" + timerSeconde.ToString() ;
+
+        if (timer == 0) return; 
+        StartCoroutine(toto());
+           
+    }
+
+}
